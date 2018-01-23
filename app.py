f1558d1be62ed7f5712ae66fe2289fda0023052d
tamilsud <34929257+tamilsud@users.noreply.github.com>
2018-01-23 13:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,72 +53,24 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "Query.Ticket":
         return {}
-    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    # yql_query = makeYqlQuery(req)
-    # if yql_query is None:
-        # return {}
-    # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    # result = urlopen(yql_url).read()
-    # data = json.loads(result)
-	ticket_id = req.get("parameters").get("number-integer")
-	print (ticket_id)
-	ticket_id=re.replace(r'\s*\"\s*\[','',ticket_id)
-	ticket_id=re.replace(r'\s*\]\s*\"','',ticket_id)
-	ticket_id=re.replace(r'\s*','',ticket_id)
-	data=dict[ticket_id]
-	print (data)
+
+    ticket_id = req.get("parameters").get("number-integer")
+    print (ticket_id)
+    ticket_id=re.replace(r'\s*\"\s*\[','',ticket_id)
+    ticket_id=re.replace(r'\s*\]\s*\"','',ticket_id)
+    ticket_id=re.replace(r'\s*','',ticket_id)
+    data=dict[ticket_id]
+    print (data)
     res = makeWebhookResult(data)
     return res
 
-
-# def makeYqlQuery(req):
-    # result = req.get("result")
-    # parameters = result.get("parameters")
-    # city = parameters.get("geo-city")
-    # if city is None:
-        # return None
-
-    # return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
 def makeWebhookResult(data):
-    # query = data.get('query')
-    # if query is None:
-        # return {}
-
-    # result = query.get('results')
-    # if result is None:
-        # return {}
-
-    # channel = result.get('channel')
-    # if channel is None:
-        # return {}
-
-    # item = channel.get('item')
-    # location = channel.get('location')
-    # units = channel.get('units')
-    # if (location is None) or (item is None) or (units is None):
-        # return {}
-
-    # condition = item.get('condition')
-    # if condition is None:
-        # return {}
-
-    # print(json.dumps(item, indent=4))
 
     speech = data
-
     print("Response:")
     print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "from a mock dictionary"
-    }
-
+    return {"speech": speech, "displayText": speech}
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
