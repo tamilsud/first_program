976fda685e6e727ad7f987bebd0d5e456c83e0ec
tamilsud <34929257+tamilsud@users.noreply.github.com>
2018-01-23 14:02:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,9 +59,10 @@ def processRequest(req):
 
     ticket_id = req.get("result").get("parameters").get("number-integer")
     print (ticket_id)
-    ticket_id=re.replace(r'\s*\[','',ticket_id)
-    ticket_id=re.replace(r'\s*\]','',ticket_id)
-    ticket_id=re.replace(r'\s*','',ticket_id)
+    ticket_id=re.sub(r'\s*\[','',ticket_id)
+    ticket_id=re.sub(r'\s*\]','',ticket_id)
+    ticket_id=re.sub(r'\s*','',ticket_id)
+    print ('ticket_id after replacement is {}'.format(ticket_id))
     data=dict[ticket_id]
     print (data)
     res = makeWebhookResult(data)
